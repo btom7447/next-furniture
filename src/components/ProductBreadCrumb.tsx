72f25b4466daab82d87697cc03b5381e740cdbed
btom7447 <tombenjamin7447@gmail.com>
2025-03-21 23:23:05 +0100

    build errors v.2.0

--- src/components/ProductBreadCrumb.tsx
+++ src/components/ProductBreadCrumb.tsx
@@ -9,11 +9,11 @@ interface ProductBreadCrumbProps {
 const ProductBreadCrumb: React.FC<ProductBreadCrumbProps> = ({ name }) => {
   return (
     <div className='my-10 w-full flex flex-wrap justify-start items-start space-x-8 space-y-5'>
-      <Link href="/" className='text-2xl text-gray-500'>Home</Link>
+      <Link href="/" className='text-lg md:text-2xl text-gray-500'>Home</Link>
       <ChevronRight size={25} />
-      <Link href="/shop" className='text-2xl text-gray-500'>Shop</Link>
+      <Link href="/shop" className='text-lg md:text-2xl text-gray-500'>Shop</Link>
       <ChevronRight size={25} /> 
-      <h6 className='text-2xl text-black pl-10 border-l-2 border-gray-500'>{name}</h6>
+      <h6 className='text-lg md:text-2xl text-black pl-10 border-l-2 border-gray-500'>{name}</h6>
     </div>
   )
 }
